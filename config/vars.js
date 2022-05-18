c0862edef5ed3273200f938a33fbaef620ad54f1
Chadwick C Simpson <mrsimpson3000@gmail.com>
2022-05-17 17:40:13 -0700

    Update vars.js
    added rejectUnauthorized: false to the connectionString of the pgConnection variable.

--- config/vars.js
+++ config/vars.js
@@ -1,3 +1,7 @@
 module.exports = {
-  pgConnection: process.env.DATABASE_URL || "postgres://postgres:s115115744@localhost:5432/my_counter"
+  pgConnection:
+  {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnautorized: false },
+  } || "postgres://postgres:s115115744@localhost:5432/my_counter"
 }
